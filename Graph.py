501d2726ec71deec59dcfa08318a9eac8bf6ec13
xenkidu <aburton85@live.com>
2020-07-24 12:12:45 -0500

    [graph] Added unit tests for the Graph class and the degree, max_degree functions.

--- Graph.py
+++ Graph.py
@@ -1,33 +1,82 @@
+"""Graph API and graph related functions."""
+import collections
+import unittest
+
+
 class Graph:
+    """Create an empty graph with V vertices, or a graph from an input file."""
 
     def __init__(self, V=None, file=None):
+        self.adj_list = collections.defaultdict(list)
+
         if file is not None:
-            pass
+            f = open(file, 'r')
+            self.Vertices = int(f.readline())
+            self.Edges = int(f.readline())
+            for line in f.readlines():
+                v, w = [int(u) for u in line.split(' ')]
+                self.add_edge(v, w)
+            f.close()
         elif V is not None:
-            pass
+            self.Vertices = V
         else:
             raise ValueError("No value was passed to constructor.")
 
+
+    # adds edge (v,w)
     def add_edge(self, v, w):
-        # adds edge (v,w)
-        pass
+        self.adj_list[v].append(w)
+        self.adj_list[w].append(v)
 
+    # returns vertices adjacent to v
     def adj(self, v):
-        # returns vertices adjacent to v
-        pass
+        return self.adj_list[v]
 
+    # number of vertices
     def V(self):
-        # number of vertices
-        pass
+        return self.Vertices
 
+    # number of edges
     def E(self):
-        # number of edges
-        pass
+        return self.Edges
 
+    # string representation
     def __str__(self):
-        # string representation
         pass
 
+
+# compute the degree of vertex v
+def degree(graph, v):
+    degree = 0
+    for w in graph.adj(v):
+        degree += 1
+    return degree
+
+
+def max_degree(graph):
+    max_ = 0
+    for v in range(graph.V()):
+        max_ = max(max_, degree(graph, v))
+    return max_
+
+
+class TestGraph(unittest.TestCase):
+    def setUp(self):
+        self.G = Graph(file='./algs4-data/tinyG.txt')
+
+    def test_create_graph_from_file(self):
+        self.assertTrue(self.G.V() == 13)
+        self.assertTrue(self.G.E() == 13)
+
+    def test_degree(self):
+        self.assertTrue(degree(self.G, 0) == 4)
+        self.assertTrue(degree(self.G, 1) == 1)
+
+    def test_max_degree(self):
+        self.assertTrue(max_degree(self.G) == 4)
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
+
+    G = Graph(file='./algs4-data/tinyG.txt')
+
